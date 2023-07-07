a8aa3e318ef5f79b0d3d13728836fe1869c94b74
LauraPenaloza <lpenaloza059@gmail.com>
2023-07-06 21:17:34 -0500

    JLabel-indices

--- Code/View/VentanaInicio.java
+++ Code/View/VentanaInicio.java
@@ -30,7 +30,7 @@ public class VentanaInicio extends JFrame implements ActionListener {
     Timer timer, timer1;
     Color[] colors = { Color.RED, Color.GREEN, Color.BLUE };
     int colorIndex = 0;
-    Color comparacion, ColorFondo, ColorFuente;
+    Color comparacion, ColorFondo, ColorFuente,ColorFondo2, ColorBoton1;
 
     
     ControladorTamagotchi controlador;
@@ -111,8 +111,10 @@ public class VentanaInicio extends JFrame implements ActionListener {
 
     public void VentPrincipal() {
         
-        ColorFondo = new Color(183, 184, 184);// 242, 239, 244
+        ColorFondo = new Color(252, 249, 221);// 242, 239, 244
         ColorFuente = new Color(59, 59, 76);
+        ColorFondo2 = new Color(254, 245, 130);
+        ColorBoton1 = new Color(233, 187, 55);
         // Propiedades del panel 1
         panelInfo = new JPanel();
         panelInfo.setBounds(0, 0, 400, 155);
@@ -120,37 +122,62 @@ public class VentanaInicio extends JFrame implements ActionListener {
         panelInfo.setBackground(ColorFondo);
 
         titulo = new JLabel("NIVEL ");
-        titulo.setBounds(140, 10, 140, 30);
+        titulo.setBounds(120, 10, 140, 30);
         titulo.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
 
-        hambre = new JLabel("Hambre ");
-        hambre.setBounds(70, 65, 100, 20);
+        nivel = new JLabel(" ");
+        nivel.setBounds(230, 10, 140, 30);
+        nivel.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
+
+        hambre = new JLabel("Hambre: ");
+        hambre.setBounds(45, 65, 100, 20);
         hambre.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
 
-        suciedad = new JLabel("Suciedad ");
-        suciedad.setBounds(70, 105, 80, 20);
+        iHambre = new JLabel("");
+        iHambre.setBounds(120, 65, 100, 20);
+        iHambre.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
+
+        suciedad = new JLabel("Suciedad: ");
+        suciedad.setBounds(40, 105, 90, 20);
         suciedad.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
 
-        sueno = new JLabel("Sueño ");
-        sueno.setBounds(230, 65, 80, 20);
+        iSuciedad = new JLabel("");
+        iSuciedad.setBounds(125, 105, 80, 20);
+        iSuciedad.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
+
+        sueno = new JLabel("Sueño: ");
+        sueno.setBounds(225, 65, 80, 20);
         sueno.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
 
-        entrenamiento = new JLabel("Entrenamiento ");
-        entrenamiento.setBounds(200, 105, 140, 20);
+        iSueno = new JLabel("");
+        iSueno.setBounds(285, 65, 80, 20);
+        iSueno.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
+
+        entrenamiento = new JLabel("Entrenamiento: ");
+        entrenamiento.setBounds(185, 105, 140, 20);
         entrenamiento.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
+
+        iEntrenamiento = new JLabel("");
+        iEntrenamiento.setBounds(315, 105, 140, 20);
+        iEntrenamiento.setFont(new Font("Trebuchet MS", Font.BOLD, 17));
 
         VentanaTag.add(panelInfo);
         panelInfo.add(titulo);
+        panelInfo.add(nivel);
         panelInfo.add(hambre);
+        panelInfo.add(iHambre);
         panelInfo.add(suciedad);
+        panelInfo.add(iSuciedad);
         panelInfo.add(sueno);
+        panelInfo.add(iSueno);
         panelInfo.add(entrenamiento);
+        panelInfo.add(iEntrenamiento);
 
         // Propiedades del panel 2
         panelAnima = new JPanel();
         panelAnima.setBounds(0, 155, 400, 315);
         panelAnima.setLayout(null);
-        panelAnima.setBackground(Color.black);
+        panelAnima.setBackground(ColorFondo2);
 
         ImageIcon gifIcon = new ImageIcon(
                 "C:\\Users\\santi\\OneDrive\\Documentos\\Proyectos Programacion\\Tamagotchi\\Recursos\\Assets\\Saludo.gif");
@@ -170,31 +197,41 @@ public class VentanaInicio extends JFrame implements ActionListener {
         comer.setBounds(60, 20, 90, 30);
         comer.addActionListener(this);
         comer.setFont(new Font("Arial", Font.BOLD, 15));
+        comer.setBackground(ColorBoton1);
         comer.setForeground(ColorFuente);
+        comer.setFocusPainted(false);
 
         dormir = new JButton("Dormir");
         dormir.setBounds(240, 20, 90, 30);
         dormir.addActionListener(this);
         dormir.setFont(new Font("Arial", Font.BOLD, 15));
+        dormir.setBackground(ColorBoton1);
         dormir.setForeground(ColorFuente);
+        dormir.setFocusPainted(false);
 
         banar = new JButton("Bañar");
         banar.setBounds(60, 65, 90, 30);
         banar.addActionListener(this);
         banar.setFont(new Font("Arial", Font.BOLD, 15));
+        banar.setBackground(ColorBoton1);
         banar.setForeground(ColorFuente);
+        banar.setFocusPainted(false);
 
         entrenar = new JButton("Entrenar");
         entrenar.setBounds(230, 65, 110, 30);
         entrenar.addActionListener(this);
         entrenar.setFont(new Font("Arial", Font.BOLD, 16));
+        entrenar.setBackground(ColorBoton1);
         entrenar.setForeground(ColorFuente);
+        entrenar.setFocusPainted(false);
 
         volver = new JButton("Volver");
         volver.setBounds(140, 120, 110, 30);
         volver.addActionListener(this);
         volver.setFont(new Font("Arial", Font.BOLD, 16));
+        volver.setBackground(ColorBoton1);
         volver.setForeground(ColorFuente);
+        volver.setFocusPainted(false);
 
         VentanaTag.add(panelAccion);
         panelAccion.add(comer);
@@ -207,6 +244,7 @@ public class VentanaInicio extends JFrame implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 titulo.setForeground(getNextColor());
+                nivel.setForeground(getNextColor());
             }
         });
         timer.start();
@@ -301,4 +339,3 @@ public class VentanaInicio extends JFrame implements ActionListener {
     
     
 }
-
